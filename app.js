5325050cb63065de07ed0b059e4b4295a86c27be
Charles Covey-Brandt <chazcb@gmail.com>
2016-02-27 12:57:58 -0800

    - separation of concerns for data, methods that update data directly, components, and init - preparing for making dom.js allow click handlers

--- app.js
+++ app.js
@@ -5,45 +5,22 @@ define('app', function (require) {
     let update = require('scripts/dom').update;
     let jsonp = require('scripts/utils').jsonp;
 
+    // -------- data ---------
+
     let DATA_STORE = {
+        saved: [],
+        savedByLink: {},
         photos: [],
         photosByLink: {}
     };
 
-    class Photo {
-        constructor(data) {
-            this.data = data;
-        }
-
-        render() {
-            return (
-                el('div', {'class': 'photo'},
-                    el('img', {src: this.data.media.m}),
-                    el('h2', {}, this.data.author)
-                )
-            )
-        }
+    function getPhotos () {
+        return DATA_STORE.photos;
     }
 
-    class App {
-        constructor(rootEl) {
-            this.rootEl = rootEl;
-        }
-
-        render() {
-            return (
-                el('div', {'class': 'photos'},
-                    DATA_STORE.photos.map((attrs) => new Photo(attrs))
-                )
-            )
-        }
-    }
-
-    let app = new App(document.getElementById('main'));
-
-    function processFeed (response) {
+    function updatePhotos (items) {
         let changed;
-        response.items.forEach((item) => {
+        items.forEach((item) => {
             if (!DATA_STORE.photosByLink[item.link]) {
                 changed = true;
                 DATA_STORE.photos.push(item);
@@ -51,16 +28,81 @@ define('app', function (require) {
             }
         });
         if (changed)
-            update(app.rootEl, app);
+            update(appElement, app);
     }
+
+    function isPhotoSaved (photo) {
+        Boolean(DATA_STORE.savedByLink[photo.link]);
+    }
+
+    function savePhoto (photo) {
+        if (isPhotoSaved(photo)) return;
+
+        DATA_STORE.saved = DATA_STORE.saved.concat(photo);
+        DATA_STORE.savedByLink[photo.link] = photo;
+        update(appElement, app);
+    }
+
+    function unsavePhoto (photo) {
+        if (!isPhotoSaved(photo)) return;
+
+        delete DATA_STORE.savedByLink[photo.link];
+        DATA_STORE.saved = DATA_STORE.saved.reduce((prev, curr) => {
+            if (curr.link !== photo.link) prev.push(curr)
+            return prev;
+        });
+        update(appElement, app);
+    }
+
+    // -------- components ----------
+
+    class Photo {
+        constructor(photo) {
+            this.photo = photo;
+        }
+
+        toggleSave() {
+            (this.isSaved() ? unsavePhoto : savePhoto)(this.photo);
+        }
+
+        isSaved() {
+            return isPhotoSaved(this.photo);
+        }
+
+        render() {
+            return (
+                el('div', {'class': 'photo'},
+                    el('img', {src: this.photo.media.m}),
+                    el('h2', {}, this.photo.author),
+                    el('button', {onClick: this.toggleSave.bind(this) }, this.isSaved() ? 'unsave' : 'save')
+                )
+            )
+        }
+    }
+
+    class App {
+        render() {
+            return (
+                el('div', {'class': 'photos'},
+                    getPhotos().map((attrs) => new Photo(attrs))
+                )
+            )
+        }
+    }
+
+    // ------------- init -------------
+
+    let appElement = document.getElementById('main');
+    let app = new App();
+
 
     jsonp(
         'http://api.flickr.com/services/feeds/photos_public.gne?format=json',
         'jsonFlickrFeed',
-        processFeed
+        (response) => updatePhotos(response.items)
     );
 
-    update(app.rootEl, app);
+    update(appElement, app);
 
     return app;
 });
